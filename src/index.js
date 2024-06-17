204b5f9576cfb7162838e0846cb95a7a29e8fa48
Cosmicgurl <165661217+Cosmicgurl@users.noreply.github.com>
2024-06-18 00:42:07 +0200

    Updated JS

--- src/index.js
+++ src/index.js
@@ -10,4 +10,20 @@ berlinElement.innerHTML = moment().format("dddd Do MMM YYYY");
 bangkokTimeElement.innerHTML = moment().tz("Asia/Bangkok").format("HH:mm:ss A");
 berlinTimeElement.innerHTML = moment().tz("Germany/Berlin").format("HH:mm:ss A");
 }
+function updateCity(event){
+    let cityTimezone = event.target.value;
+		let cityTime = moment().tz(cityTimezone);
+		let citiesElement = document.querySelector("#cities");
+		citiesElement.innerHTML= `
+		    <div class="city">
+        <div>
+        <h2 >${cityTimezone}</h2>
+        <div class="cityDate" id="berlin">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+        <div class="cityTime" id="berlinTime"> ${cityTime.format("MMMM Do YYYY")}<small></small></div>
+    </div>`
+}
 setInterval( updateTime, 1000);
+
+let selectElement = document.querySelector("#city");
+selectElement.addEventListener("change", updateCity)
